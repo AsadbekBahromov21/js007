3e9527aa53163bf95b2c8a6e0fb2ddf447115e57
AsadbekBahromov21 <bahromov1621a@gmail.com>
2024-06-14 21:25:51 +0300

    js007

---
+++ js/app.js
@@ -0,0 +1,206 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    stagePadding: 50,
+    dots: false,
+    responsive: {
+        0: {
+            items: 3
+        },
+        600: {
+            items: 4
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
+
+
+
+// {
+//     // case1 masala
+//     function Dayneme(n) {
+//         switch (n) {
+//             case 1:
+//                 return "Duyshanba"
+//             case 2:
+//                 return "Seyshanba"
+//             case 3:
+//                 return "chorshanba"
+//             case 4:
+//                 return "Payshanba"
+//             case 5:
+//                 return "Juma"
+//             case 6:
+//                 return "Shanba"
+//             case 7:
+//                 return "Yakshanba"
+//             default:
+//                 return  "Bunday hafta kuni yo"
+//         }
+//     }
+//     console.log(Dayneme(1));
+// }
+// {
+//     // case 2 masla
+//     function Pirce(n){
+//         switch (n){
+//             case 1:
+//             return "yomon"
+//              case 2:
+//             return "qoniqarsiz"
+//              case 3:
+//             return "qoniqarli"
+//              case 4:
+//             return "yaxshi"
+//              case 5:
+//             return "alo"
+//             default:
+//                 return "xato"
+//         }
+//     }
+//     console.log(Pirce(6));
+// }
+// {
+//     // case 3 masla
+//     function moon(n) {
+//         switch (n) {
+//             case 1:
+//                 return " 1 oy qish"
+//             case 2:
+//                 return "2 oy qish"
+//             case 3:
+//                 return "3 oy qish"
+//             case 4:
+//                 return "4 oy baxor"
+//             case 5:
+//                 return "5 oy baxor"
+//             case 6:
+//                 return "6 oy baxor"
+//             case 7:
+//                 return "7 oy yoz"
+//             case 8:
+//                 return "8 oy yoz"
+//             case 9:
+//                 return "9 oy yoz"
+//             case 10:
+//                 return "10 oy kuz"
+//             case 11:
+//                 return "11 oy kuz"
+//             case 12:
+//                 return "12 oy kuz"
+
+//             default:
+//                 return "bunday oy yoq"
+
+//         }
+//     }
+//     console.log(moon(6));
+// }
+
+// {
+//     // case 4 masla
+//     function moon(n) {
+//         switch (n) {
+//             case 4:
+//             case 6:
+//             case 9:
+//             case 11:
+//                 return "30 kundan iborat"
+//             case 1:
+//             case 3:
+//             case 5:
+//             case 7:
+//             case 8:
+//             case 9:
+//             case 10:
+//                 return "31 kundan iborat"
+//             case 2:
+//                 return "28 kundan iborat"
+//             default:
+//                 return "bunday oy kunlari yo"
+//         }
+//     }
+//     console.log(moon(4));
+// }
+
+
+
+// {
+//     // case 5 masala
+//     function son(a, b, amal) {
+//         let sum;
+//         switch (amal) {
+//             case 1:
+//                 sum = a + b;
+//                 break;
+//             case 2:
+//                 sum = a - b;
+//                 break
+//             case 3:
+//                 sum = a / b;
+//                 break;
+//             case 4:
+//                 sum = a * b;
+//                 break;
+
+//         }
+//         console.log(sum);
+//     }
+//     console.log(son(6, 3, 3));
+// }
+
+
+// {
+//     //case  6 masala
+//     function olchov(n,) {
+//         switch (n) {
+//             case 1:
+//                 return "1 dm : 0,1 m"
+//             case 2:
+//                 return "1 km; 2000 m"
+//             case 3:
+//                 return "3 m"
+//             case 4:
+//                 return "4 mm; 0,001mm"
+//             case 5:
+//                 return "5 sm; 0.05 m"
+//         }
+//     }
+//     console.log(olchov(5));
+
+// }
+
+// {
+//     // case 7 masala
+//     function ogirlik(n, amal) {
+//         let sum;
+//         switch (amal) {
+//             case 1:
+//                 sum = n * 1;
+//                 break;
+//             case 2:
+//                 sum = n / 1000;
+//                 break;
+//             case 3:
+//                 sum = n / 1000;
+//                 break;
+//             case 4:
+//                 sum = n * 1000;
+//                 break;
+//             case 5:
+//                 sum = n * 100;
+//                 break;
+//                 default:
+//                     sum = "hech narsa yo";
+
+//         }
+//         console.log(sum , "kg");
+//     }
+//     console.log(ogirlik(2, 4));
+// }
+
